60cb6a46411a9dba51c2f75f3b8eac1c0242a33e
Jason Skuby <jskuby@gmail.com>
2020-01-14 20:29:06 -0600

    Commit WIP

--- NETStandardLibrary.Linq/ExpressionMethods.cs
+++ NETStandardLibrary.Linq/ExpressionMethods.cs
@@ -1,10 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace NETStandardLibrary.Linq
 {
+	// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/expression-trees/how-to-use-expression-trees-to-build-dynamic-queries
 	public sealed class ExpressionMethods
 	{
+		public static Expression<Func<T, bool>> ToIQueryableExpression<T, U>(
+			string propertyName,
+			U value,
+			IQueryableExpressionType expressionType)
+		{
+			var parameter = Expression.Parameter(typeof(T), "type");
+			var property = Expression.Property(parameter, propertyName);
+			var constant = Expression.Constant(value, typeof(U));
+
+			if (typeof(U) == typeof(string))
+			{
+				// If it's a string, limit to only Contains and Equals
+				switch (expressionType)
+				{
+					case IQueryableExpressionType.Contains:
+						var method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+						var containsMethodCall = Expression.Call(parameter, method, constant);
+						return Expression.Lambda<Func<T, bool>>(containsMethodCall, parameter);
+					case IQueryableExpressionType.Equal:
+						// Expression.Equal()
+						break;
+					default:
+						throw new NotImplementedException("Strings are limited to Contains and Equals expression types only");
+				}
+			}
+			else
+			{
+				switch (expressionType)
+				{
+					case IQueryableExpressionType.Equal:
+						// Expression.Equal();
+						break;
+					case IQueryableExpressionType.GreaterThan:
+						// Expression.GreaterThan();
+						break;
+					case IQueryableExpressionType.GreaterThanOrEqual:
+						// Expression.GreaterThanOrEqual();
+						break;
+					case IQueryableExpressionType.LessThan:
+						// Expression.LessThan();
+						break;
+					case IQueryableExpressionType.LessThanOrEqual:
+						// Expression.LessThanOrEqual();
+						break;
+					default:
+						throw new NotImplementedException($"The expression type {expressionType} is not implemented");
+				}
+			}
+		}
+
 		// TODO: Cleanup?
 		// public static Expression<Func<Type, object>> ToPropertyExpression(Type type, string propertyName)
 		// {
@@ -13,6 +66,35 @@ namespace NETStandardLibrary.Linq
 		// 	var propAsObject = Expression.Convert(property, typeof(object));
 		// 	return Expression.Lambda<Func<Type, object>>(propAsObject, parameter);
 		// }
+
+		public static MethodCallExpression ToMethodCallExpression<T>(
+			IQueryable<T> query,
+			string propertyName,
+			string methodName,
+			IComparer<object> comparer = null)
+		{
+			var parameter = Expression.Parameter(typeof(T), "x");
+			var body = propertyName
+				.Split('.')
+				.Aggregate<string, Expression>(parameter, Expression.PropertyOrField);
+
+			return comparer != null
+				? Expression.Call(
+						typeof(Queryable),
+						methodName,
+						new[] { typeof(T), body.Type },
+						query.Expression,
+						Expression.Lambda(body, parameter),
+						Expression.Constant(comparer)
+					)
+				: Expression.Call(
+						typeof(Queryable),
+						methodName,
+						new[] { typeof(T), body.Type },
+						query.Expression,
+						Expression.Lambda(body, parameter)
+					);
+		}
 
 		public static Expression<Func<T, object>> ToPropertyExpression<T>(string propertyName)
 		{
--- NETStandardLibrary.Linq/IQueryableExtensions.cs
+++ NETStandardLibrary.Linq/IQueryableExtensions.cs
@@ -1,45 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace NETStandardLibrary.Linq
 {
 	public static class IQueryableExtensions
 	{
-		/// <summary>
-		/// Builds the Queryable functions using a TSource property name.
-		/// </summary>
-		public static IOrderedQueryable<T> CallOrderedQueryable<T>(this IQueryable<T> query, string methodName, string propertyName,
-						IComparer<object> comparer = null)
-		{
-				var param = Expression.Parameter(typeof(T), "x");
-				var body = propertyName
-					.Split('.')
-					.Aggregate<string, Expression>(param, Expression.PropertyOrField);
-
-				return comparer != null
-					? (IOrderedQueryable<T>)query.Provider.CreateQuery(
-						Expression.Call(
-							typeof(Queryable),
-							methodName,
-							new[] { typeof(T), body.Type },
-							query.Expression,
-							Expression.Lambda(body, param),
-							Expression.Constant(comparer)
-						)
-					)
-					: (IOrderedQueryable<T>)query.Provider.CreateQuery(
-						Expression.Call(
-							typeof(Queryable),
-							methodName,
-							new[] { typeof(T), body.Type },
-							query.Expression,
-							Expression.Lambda(body, param)
-						)
-					);
-		}
-
 		/// <summary>
 		/// Retuns a page of data using <c>IQueryable.Skip</c> and <c> IQueryable.Take</c>.
 		/// </summary>
@@ -137,6 +103,19 @@ namespace NETStandardLibrary.Linq
 		public static IOrderedQueryable<T> ThenByDescending<T>(this IOrderedQueryable<T> query, string propertyName, IComparer<object> comparer = null)
 		{
 			return CallOrderedQueryable(query, "ThenByDescending", propertyName, comparer);
+		}
+
+		/// <summary>
+		/// Builds the Queryable functions using a TSource property name.
+		/// </summary>
+		private static IOrderedQueryable<T> CallOrderedQueryable<T>(
+			this IQueryable<T> query,
+			string methodName,
+			string propertyName,
+			IComparer<object> comparer = null)
+		{
+			var methodCallExpression = ExpressionMethods.ToMethodCallExpression(query, propertyName, methodName, comparer);
+			return (IOrderedQueryable<T>)query.Provider.CreateQuery(methodCallExpression);
 		}
 	}
 }

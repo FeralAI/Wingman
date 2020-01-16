40ea12af27eec835e78efdd24c8f9fa4a5712384
Jason Skuby <jskuby@users.noreply.github.com>
2020-01-16 08:21:48 -0600

    Prep for more work

--- src/NETStandardLibrary.Linq/ExpressionMethods.cs
+++ src/NETStandardLibrary.Linq/ExpressionMethods.cs
@@ -53,11 +53,21 @@ namespace NETStandardLibrary.Linq
 			Type valueType,
 			WhereClauseType expressionType)
 		{
+			if (string.IsNullOrWhiteSpace(propertyName))
+				throw new ArgumentNullException("The propertyName must not be null or empty");
+
+			if (valueType == null)
+				throw new ArgumentNullException("The valueType must be provided");
+
 			var parameter = Expression.Parameter(typeof(T), "type");
-			var property = propertyName
-				.Split('.')
-				.Aggregate<string, Expression>(parameter, Expression.PropertyOrField);
 			var constant = Expression.Constant(value, valueType);
+
+			var propertyParts = propertyName.Split('.');
+			var property = propertyParts.Aggregate<string, Expression>(parameter, Expression.PropertyOrField);
+
+			// TODO: Maybe move this into a helper function
+			var notNull = Expression.NotEqual(property, Expression.Constant(null, property.Type));
+			var notNullPredicate = Expression.Lambda<Func<T, bool>>(notNull, parameter);
 
 			if (valueType == typeof(string))
 			{
@@ -68,9 +78,11 @@ namespace NETStandardLibrary.Linq
 						var method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
 						var containsMethodCall = Expression.Call(property, method, constant);
 						return Expression.Lambda<Func<T, bool>>(containsMethodCall, new ParameterExpression[] { parameter });
+
 					case WhereClauseType.Equal:
 						var equalExpression = Expression.Equal(property, constant);
 						return Expression.Lambda<Func<T, bool>>(equalExpression, new ParameterExpression[] { parameter });
+
 					default:
 						throw new NotImplementedException("Strings are limited to Contains and Equals expression types only");
 				}
@@ -82,18 +94,23 @@ namespace NETStandardLibrary.Linq
 					case WhereClauseType.Equal:
 						var equalExpression = Expression.Equal(property, constant);
 						return Expression.Lambda<Func<T, bool>>(equalExpression, new ParameterExpression[] { parameter });
+
 					case WhereClauseType.GreaterThan:
 						var greaterThanExpression = Expression.GreaterThan(property, constant);
 						return Expression.Lambda<Func<T, bool>>(greaterThanExpression, new ParameterExpression[] { parameter });
+
 					case WhereClauseType.GreaterThanOrEqual:
 						var greaterThanOrEqualExpression = Expression.GreaterThanOrEqual(property, constant);
 						return Expression.Lambda<Func<T, bool>>(greaterThanOrEqualExpression, new ParameterExpression[] { parameter });
+
 					case WhereClauseType.LessThan:
 						var lessThanExpression = Expression.LessThan(property, constant);
 						return Expression.Lambda<Func<T, bool>>(lessThanExpression, new ParameterExpression[] { parameter });
+
 					case WhereClauseType.LessThanOrEqual:
 						var lessThanOrEqualExpression = Expression.LessThanOrEqual(property, constant);
 						return Expression.Lambda<Func<T, bool>>(lessThanOrEqualExpression, new ParameterExpression[] { parameter });
+
 					default:
 						throw new NotImplementedException($"The expression type {expressionType} is not implemented");
 				}

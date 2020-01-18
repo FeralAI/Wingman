1ac9bcf79d96c8e33a119b31f263c397c1f3808f
Jason Skuby <jskuby@users.noreply.github.com>
2020-01-18 01:23:39 -0600

    More test cases

--- tests/NETStandardLibraryTests.Common/TypeExtensionsTests.cs
+++ tests/NETStandardLibraryTests.Common/TypeExtensionsTests.cs
@@ -14,6 +14,7 @@ namespace NETStandardLibraryTests.Common
 		[InlineData(typeof(DateTime?), true)]
 		[InlineData(typeof(int), true)]
 		[InlineData(typeof(int?), true)]
+		[InlineData(typeof(object), false)]
 		[InlineData(typeof(string), true)]
 		public void IsComparable(Type type, bool expected)
 		{
@@ -21,12 +22,24 @@ namespace NETStandardLibraryTests.Common
 		}
 
 		[Theory]
+		[InlineData(typeof(byte?), true)]
+		[InlineData(typeof(byte), true)]
+		[InlineData(typeof(sbyte), true)]
+		[InlineData(typeof(short), true)]
+		[InlineData(typeof(ushort), true)]
+		[InlineData(typeof(int), true)]
+		[InlineData(typeof(uint), true)]
+		[InlineData(typeof(long), true)]
+		[InlineData(typeof(ulong), true)]
+		[InlineData(typeof(float), true)]
+		[InlineData(typeof(double), true)]
+		[InlineData(typeof(decimal), true)]
+
 		[InlineData(typeof(List<int>), false)]
+		[InlineData(typeof(bool?), false)]
 		[InlineData(typeof(bool), false)]
 		[InlineData(typeof(DateTime), false)]
-		[InlineData(typeof(DateTime?), false)]
-		[InlineData(typeof(int), true)]
-		[InlineData(typeof(int?), true)]
+		[InlineData(typeof(object), false)]
 		[InlineData(typeof(string), false)]
 		public void IsNumericType(Type type, bool expected)
 		{

29be41424651bddd25ac26cedb71e900517c2dd7
Jason Skuby <jskuby@gmail.com>
2020-01-22 20:38:16 -0600

    Add email class sample

---
+++ samples/NETStandardSamples.Web/Pages/Email/EmailServiceEmail.cs
@@ -0,0 +1,8 @@
+namespace NETStandardSamples.Web.Pages.Email
+{
+	public class EmailServiceEmail : NETStandardLibrary.Email.Email
+	{
+		public string Greeting { get; set; }
+		public string Name { get; set; }
+	}
+}

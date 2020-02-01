d6d00a883db5baa2f027b8e701fa4ecd7b175de0
Jason Skuby <jskuby@users.noreply.github.com>
2020-02-01 09:11:25 -0600

    Add logging

--- samples/NETStandardSamples.Web/Controllers/PeopleController.cs
+++ samples/NETStandardSamples.Web/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NETStandardSamples.Web.Services;
 
 namespace NETStandardSamples.Web.Controllers
@@ -7,10 +8,12 @@ namespace NETStandardSamples.Web.Controllers
 	[Route("api/v{version:apiVersion}/people")]
 	public partial class PeopleController : ApiController
 	{
+		private readonly ILogger<PeopleController> logger;
 		private readonly TestPersonService personService;
 
-		public PeopleController(TestPersonService personService)
+		public PeopleController(ILogger<PeopleController> logger, TestPersonService personService)
 		{
+			this.logger = logger;
 			this.personService = personService;
 		}
 	}

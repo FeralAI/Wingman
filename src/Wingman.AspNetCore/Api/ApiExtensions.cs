ce11bffa39fb9c67d5a629acdf199f9875f57046
Jason Skuby <jskuby@users.noreply.github.com>
2020-02-23 10:15:02 -0600

    Cleanup

--- src/Wingman.AspNetCore/Api/ApiExtensions.cs
+++ src/Wingman.AspNetCore/Api/ApiExtensions.cs
@@ -5,11 +5,20 @@ namespace Wingman.AspNetCore.Api
 {
 	public static class ApiExtensions
 	{
+		/// <summary>
+		/// Adds versioning to a WebApi application.
+		/// </summary>
+		/// <remarks>
+		/// API versioning and documentation:
+		/// https://github.com/microsoft/aspnet-api-versioning/tree/master/samples/aspnetcore/SwaggerSample
+		/// https://github.com/microsoft/aspnet-api-versioning/wiki/Versioning-via-the-URL-Path
+		/// </remarks>
+		/// <param name="this">The service collection</param>
+		/// <param name="defaultVersion">The default API version to serve</param>
+		/// <param name="nameFormat">The API version format</param>
+		/// <returns>The service collection</returns>
 		public static IServiceCollection AddVersionedApi(this IServiceCollection @this, ApiVersion defaultVersion, string nameFormat = "VVVV")
 		{
-			// API versioning and documentation
-			// https://github.com/microsoft/aspnet-api-versioning/tree/master/samples/aspnetcore/SwaggerSample
-			// https://github.com/microsoft/aspnet-api-versioning/wiki/Versioning-via-the-URL-Path
 			@this.AddApiVersioning(options => {
 				options.ReportApiVersions = true;
 				options.AssumeDefaultVersionWhenUnspecified = true;
--- src/Wingman.AspNetCore/Api/ApiResult.cs
+++ src/Wingman.AspNetCore/Api/ApiResult.cs
@@ -1,53 +1,53 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Wingman.AspNetCore.Api
 {
 	/// <summary>
-	/// Basic API result class which will typically represent an error state.
+	/// Basic API result class which typically represents an error state and defaults to a 400 status.
 	/// </summary>
 	public class ApiResult
 	{
-		public ApiResult(string message = null, IEnumerable<string> errors = null, int statusCode = StatusCodes.Status400BadRequest)
+		public ApiResult(string message = null, IEnumerable<string> errors = null, int status = StatusCodes.Status400BadRequest)
 		{
 			Message = message;
-			StatusCode = statusCode;
+			Status = status;
 			Errors = errors;
 		}
 
-		public ApiResult(string message = null, ModelStateDictionary modelState = null, int statusCode = StatusCodes.Status400BadRequest)
+		public ApiResult(string message = null, ModelStateDictionary modelState = null, int status = StatusCodes.Status400BadRequest)
 		{
 			Message = message;
-			StatusCode = statusCode;
-			Errors = modelState.SelectMany(kv => kv.Value.Errors.Select(e => e.ErrorMessage));
+			Status = status;
+			Errors = modelState?.SelectMany(kv => kv.Value.Errors?.Select(e => e.ErrorMessage));
 		}
 
 		public string Message { get; set; }
-		public int StatusCode { get; set; }
+		public int Status { get; set; }
 		public IEnumerable<string> Errors { get; set; }
 	}
 
 	/// <summary>
-	/// Basic API result class which will typically represent a success state with a value of the generic type parameter.
+	/// Basic API result class which typically represents a success state and defaults to a 200 status with a value of the generic
+	/// type parameter.
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
 	public class ApiResult<T> : ApiResult
 	{
-		public ApiResult(T result, string message = null, IEnumerable<string> errors = null, int statusCode = StatusCodes.Status200OK)
-			: base(message, errors, statusCode)
+		public ApiResult(T value, string message = null, IEnumerable<string> errors = null, int status = StatusCodes.Status200OK)
+			: base(message, errors, status)
 		{
-			Result = result;
+			Value = value;
 		}
 
-		public ApiResult(T result, string message = null, ModelStateDictionary errors = null, int statusCode = StatusCodes.Status400BadRequest)
-			: base(message, errors, statusCode)
+		public ApiResult(T value, string message = null, ModelStateDictionary modelState = null, int status = StatusCodes.Status200OK)
+			: base(message, modelState, status)
 		{
-			Result = result;
+			Value = value;
 		}
 
-		public T Result { get; set; }
+		public T Value { get; set; }
 	}
 }
